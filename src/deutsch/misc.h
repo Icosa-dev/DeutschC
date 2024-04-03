b6d3ea5f5fcd52f4de60faa5a14e35ecc27b00f3
Icosa <117495728+H4drian@users.noreply.github.com>
2024-04-03 19:36:37 -0400

    Update misc.h

--- src/deutsch/misc.h
+++ src/deutsch/misc.h
@@ -1,11 +1,6 @@
 // macro definition for miscellaneous keywords translated to german
 // makrodefinition fur verschiedene Schlusselworter ubersetzt ins deutsche
 
-#ifdef _DEUTSCH_MISC_H
-    #error "deutsch/misc.h included twice"
-    #error "deutsch/misc.h zweimal enthalten"
-#endif
-
 #ifndef _DEUTSCH_MISC_H
     #define _DEUTSCH_MISC_H
 
