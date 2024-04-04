22ea0bf324212ea0965d9afbe95f218487fa8ebe
Icosa <117495728+H4drian@users.noreply.github.com>
2024-04-04 15:15:09 -0400

    Update misc.h

--- src/deutsch/misc.h
+++ src/deutsch/misc.h
@@ -1,8 +1,8 @@
 // macro definition for miscellaneous keywords translated to german
 // makrodefinition fur verschiedene Schlusselworter ubersetzt ins deutsche
 
-#ifndef _DEUTSCH_MISC_H
-    #define _DEUTSCH_MISC_H
+#ifndef DEUTSCH_MISC_H
+    #define DEUTSCH_MISC_H
 
     #ifndef registrieren
         #define registrieren register
